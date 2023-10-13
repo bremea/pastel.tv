58cd16b618c6fd8da9dd53300a2cfe9cd02145fb
bremea <bremeagames@gmail.com>
2023-10-13 16:10:21 -0400

    fonts, colors, styles

---
+++ web/src/lib/components/input/TextInput.svelte
@@ -0,0 +1,5 @@
+<input
+	class="bg-transparent border-white border-opacity-25 border-2 rounded-full px-4 py-1 transition-all focus:border-vivid-sky-blue"
+	placeholder="Email"
+	type="text"
+/>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TextInput from "$lib/components/input/TextInput.svelte";
+
+</script>
+<div class="w-full h-screen fancy-bg pb-2">
+	<div class="w-full h-full flex items-center justify-center bg-night">
+		<div class="space-y-4 flex flex-col items-center">
+			<h1 class="text-vivid-sky-blue">skyscreen.tv</h1>
+			<p>Enter your email to continue</p>
+			<TextInput />
+		</div>
+	</div>
+</div>
